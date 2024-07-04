c4e6abf3eda669ddb451fd58aa229954bd22348d
A7-T62 <109889758+Diegoson@users.noreply.github.com>
2024-07-04 09:16:04 +0200

    A7-T62

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authenticate = require('./middleware/authenticate');
+const User = require('./models/user');
+const userRoutes = require('./routes/user');
+const generateApiKey = require('./utils/generateApiKey');
+
+const app = express();
+mongoose.connect('mongodb://localhost:27017/yourDatabaseName', { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(bodyParser.json());
+
+app.use('/api', userRoutes);
+
+app.get('/api/search/yt', authenticate, async (req, res) => {
+  const query = req.query.query;
+  let apiKey = req.query.api_key; // Assuming API key is sent as a query parameter
+
+  if (!query) {
+    return res.status(400).json({ error: 'Query parameter "query" is required' });
+  }
+
+  try {
+    if (!apiKey || apiKey !== 'Diegoson') {
+      apiKey = generateApiKey();       
+    }
+
+     const data = await searchYouTube(query);
+    return res.json({ result: data, api_key: apiKey });
+  } catch (e) {
+    return res.status(500).json({ error: e.message });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
