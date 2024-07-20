39b30f5ca21a4f3d95382ef8bfad91d6defd8295
A7-T62 <109889758+Diegoson@users.noreply.github.com>
2024-07-20 22:30:17 +0200

    🐊

--- routes/astrid_apis.js
+++ routes/astrid_apis.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const config = require('../config.json');
 
+router.get('/lyrics/:title/:artist', async (req, res) => {
+  try {
+    const { title, artist } = req.params;
+    const data = await geniusLyric(title, artist);
+    res.json({
+      data,
+      apiOwner: config.NAME
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Error' });
+  }
+});
+
+module.exports = router;
