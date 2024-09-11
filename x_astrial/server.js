81c355ddcee90b362b89f989e746300b22ef1d87
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-09-11 20:15:31 +0200

    🤺

--- x_astrial/server.js
+++ x_astrial/server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000; 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+const owner_name = 'XAstrial';
 const fb_dl = require('./function').fb_dl;
 app.get('/api/fb_dl', async (req, res) => {
     const url = req.query.url;
@@ -14,9 +14,9 @@ app.get('/api/fb_dl', async (req, res) => {
         return res.status(400).json({ error: 'url parameter is required' });
     } try {
         const results = await fb_dl(url); 
-        res.json(results);
+        res.json({ owner_name, results);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(500).json({ error: error.message, owner_name });
     }});
 app.use(express.static('public')); 
 app.listen(port, () => {
